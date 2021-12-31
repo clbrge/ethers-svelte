bbe57b3f2a3fceaa91d104a69b1d2b6213dc63cc
Christophe Le Bars <clb@toort.net>
2021-12-31 13:19:22 +0100

    update example

--- example/src/routes/ethers.svelte
+++ example/src/routes/ethers.svelte
@@ -3,75 +3,88 @@
   import { ethers } from 'ethers'
   import { connected, provider, signer, defaultEvmStores } from 'svelte-ethers-store'
 
-
-  //import { provider, signer } from 'ethers'
+  let type
+  let pending = false
 
   const connect = async () => {
+    pending = true
+    try {
+      const handler = {
+        Browser: () => defaultEvmStores.setBrowserProvider(),
+        RPC: () => defaultEvmStores.setProvider('https://rpc.xdaichain.com/'),
+        Infura: () => defaultEvmStores.setProvider(new ethers.providers.InfuraProvider("ropsten")),
+        Etherscan: () => defaultEvmStores.setProvider(new ethers.providers.EtherscanProvider("rinkeby")),
+        Alchemy: () => defaultEvmStores.setProvider(new ethers.providers.AlchemyProvider("ropsten")),
+        Clouflare: () => defaultEvmStores.setProvider(new ethers.providers.CloudflareProvider()),
+      }
 
-    const x = new ethers.providers.Web3Provider( window.ethereum )
+      await handler[type]()
 
-    //new ethers.providers.InfuraProvider("ropsten")
-    //new ethers.providers.EtherscanProvider("rinkeby")
-    //new ethers.providers.AlchemyProvider("ropsten")
-    //new ethers.providers.CloudflareProvider()
-
-    // await defaultEvmStores.setBrowserProvider()
-
-    await defaultEvmStores.setProvider(  x  )
-
-    console.log('$connected', defaultEvmStores.$connected  )
-    console.log('$provider', defaultEvmStores.$provider  )
-    console.log('$signer', defaultEvmStores.$signer  )
-
-
+      console.log('$connected', defaultEvmStores.$connected  )
+      console.log('$provider', defaultEvmStores.$provider  )
+      console.log('$signer', defaultEvmStores.$signer  )
+    } catch(e) {
+      console.log(e)
+      pending = false
+    }
   }
+
   const disconnect = async () => {
-    console.log('connect', defaultEvmStores )
     await defaultEvmStores.disconnect()
+    pending = false
   }
 
   $: network = $connected ? $provider.getNetwork() : ''
 
-
-
 </script>
 
 
-  <svelte:head>
-	<title>About</title>
+<svelte:head>
+  <title>About</title>
 </svelte:head>
 
 <div class="content">
-	<h1>About svelte-ethers-store</h1>
+  <h1>About svelte-ethers-store</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-
-
-    {#if $connected}
-
-	<p>
-	  Well done, you are now connected to the blockchain
-
-      {#await network}
-      <span>waiting...</span>
-      {:then value}
-      <span>{JSON.stringify(value)}</span>
-      {/await}
-
-    </p>
+  <p>
+	This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+	following into your command line and following the prompts:
+  </p>
 
 
-    <button on:click={disconnect}> Disconnect </button>
 
-    {:else}
+  {#if $connected}
 
-    <button on:click={connect}> Connect </button>
+  <p>
+	Well done, you are now connected to the blockchain
 
-    {/if}
+    {#await network}
+    <span>waiting...</span>
+    {:then value}
+    <span>{JSON.stringify(value)}</span>
+    {/await}
+
+  </p>
+
+
+  <button on:click={disconnect}> Disconnect </button>
+
+  {:else}
+
+  <p>Choose the provider:</p>
+  <select bind:value={type}>
+    <option value="Browser">Browser (window.ethereum)</option>
+    <option value="RPC">https://rpc.xdaichain.com/ (RPC)</option>
+    <option value="Infura">ethers.providers.InfuraProvider('ropsten')</option>
+    <option value="Etherscan">ethers.providers.EtherscanProvider('rinkeby')</option>
+    <option value="Alchemy">ethers.providers.AlchemyProvider('ropsten')</option>
+    <option value="Clouflare">ethers.providers.CloudflareProvider()</option>
+  </select>
+
+  <button disabled={pending} on:click={connect}>Connect with {type}</button>
+  {#if pending}connecting...{/if}
+
+  {/if}
 
 
 </div>
