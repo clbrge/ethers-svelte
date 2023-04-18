08cbb7396b50d3e906161927a9f5bdc0b876abc1
Christophe Le Bars <clb@toort.net>
2023-04-18 09:27:19 +0200

    force cjs extension + proxied - rollup 3.20.4

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,7 +5,7 @@ export default [
     input: "./src/stores.js",
     output: [
       { file: "dist/index.mjs", format: "es" },
-      { file: "dist/index.js", format: "umd", name: "ethers-stores" },
+      { file: "dist/index.cjs", format: "umd", name: "ethers-stores" },
     ],
   },
   {
